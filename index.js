ba7948df24b2128b3d58e69c2f23469bc5e67dfc
Jake Nelson <jnelson180@gmail.com>
2017-09-24 20:26:52 -0500

    heroku building...

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
+require('dotenv').config();
 var express = require('express');
 var fetch = require('node-fetch');
 var https = require('https');
-require('dotenv').config();
 var parseString = require('xml2js').parseString;
 var app = express();
 const key = process.env["GOODREADS_KEY"];
 
-var zlib = require('zlib');
-
-// app.use(require('express-decompress').create());
-
 app.get('/', function (req, res) {
+        console.log(req.query);
         const id = req.query.id;
         const shelf = req.query.shelf;
         const sort = req.query.sort;
         const perPage = req.query.per_page;
 
         var reviews = null;
-        // console.log(req.query);
+
         https.get("https://www.goodreads.com/review/list?v=2&id=" + id + "&shelf=" + shelf + 
             "&sort=" + sort + "&key=" + key + "&per_page=" + perPage, 
             (response) => {
-                console.log(response);
+                // console.log(response);
                 response.setEncoding('utf8');
                 let rawData = '';
                 response.on('data', (chunk) => { rawData += chunk; });
@@ -29,7 +26,7 @@ app.get('/', function (req, res) {
                     try {
                         parseString(rawData, function (err, r) {
                             let rev = r.GoodreadsResponse.reviews[0].review;
-                            console.dir(rev);
+                            // console.dir(rev);
                             reviews = rev;
                             res.send(rev);
                         });
@@ -38,8 +35,8 @@ app.get('/', function (req, res) {
                     }
                 });
             }).on('error', (e) => {
+                console.error(`Got error: ${e.message}`);
                 res.send(reviews);
-            console.error(`Got error: ${e.message}`);
             });
     });
         
