1e84604de07a9cc43e347c6973009dad52237059
Jake Nelson <jnelson180@gmail.com>
2017-09-29 12:39:52 -0500

    Debugging

--- index.js
+++ index.js
@@ -72,37 +72,37 @@ app.get('/api', function (req, res) {
     });        
 })
 
-app.get('/', function (req, res) {
-        const id = req.query.id;
-        const shelf = req.query.shelf;
-        const sort = req.query.sort;
-        const perPage = req.query.per_page;
+// app.get('/', function (req, res) {
+//         const id = req.query.id;
+//         const shelf = req.query.shelf;
+//         const sort = req.query.sort;
+//         const perPage = req.query.per_page;
 
-        var reviews = null;
+//         var reviews = null;
 
-        https.get("https://www.goodreads.com/review/list?v=2&id=" + id + "&shelf=" + shelf + 
-            "&sort=" + sort + "&key=" + key + "&per_page=" + perPage, 
-            (response) => {
-                response.setEncoding('utf8');
-                let rawData = '';
-                response.on('data', (chunk) => { rawData += chunk; });
-                response.on('end', () => {
-                    try {
-                        parseString(rawData, function (err, r) {
-                            let rev = r.GoodreadsResponse.reviews[0].review;
-                            console.dir(rev);
-                            reviews = rev;
-                            res.send(rev);
-                        });
-                    } catch (e) {
-                    console.error('e:', e.message);
-                    }
-                });
-            }).on('error', (e) => {
-                console.error(`Got error: ${e.message}`);
-                res.send(reviews);
-            });
-    });
+//         https.get("https://www.goodreads.com/review/list?v=2&id=" + id + "&shelf=" + shelf + 
+//             "&sort=" + sort + "&key=" + key + "&per_page=" + perPage, 
+//             (response) => {
+//                 response.setEncoding('utf8');
+//                 let rawData = '';
+//                 response.on('data', (chunk) => { rawData += chunk; });
+//                 response.on('end', () => {
+//                     try {
+//                         parseString(rawData, function (err, r) {
+//                             let rev = r.GoodreadsResponse.reviews[0].review;
+//                             console.dir(rev);
+//                             reviews = rev;
+//                             res.send(rev);
+//                         });
+//                     } catch (e) {
+//                     console.error('e:', e.message);
+//                     }
+//                 });
+//             }).on('error', (e) => {
+//                 console.error(`Got error: ${e.message}`);
+//                 res.send(reviews);
+//             });
+//     });
         
 console.log('App listening on port', port);
 app.listen(port);
