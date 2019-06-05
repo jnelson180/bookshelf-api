a3c89f83706c53c0a9c5e40e3883447560f4e9f8
Jake Nelson <jnelson180@gmail.com>
2019-06-05 06:43:02 -0500

    Working index-new for scraping and data aggregation into SQL import format

--- index-new.js
+++ index-new.js
@@ -22,7 +22,9 @@ var url = 'mongodb://' + dbUser + ':' + dbPassword + '@ds155934.mlab.com:55934/b
 app.use(cors());
 app.use(bodyParser.xml({ limit: '16mb' }));
 
-// https://localhost:3000/?id=78840638&shelf=read&perPage=20
+const removeSymbols = (str) => str.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+
+// url format: https://localhost:3000/?id=78840638&shelf=read&perPage=20
 const getReviews = (req, res, id, shelf, sort, perPage, page) => {
     let allReviews = [];
     let allBooks = [];
@@ -37,40 +39,31 @@ const getReviews = (req, res, id, shelf, sort, perPage, page) => {
             response.on('end', () => {
                 try {
                     parseString(rawData, function (err, r) {
-                        console.log(r);
                         if (r.html) {
-                            console.dir(r.html.body.div)
+                            // enable for debug; some results are not what is expected
+                            console.dir(r.html.body.div);
                         }
+
                         const data = r.GoodreadsResponse.reviews[0];
-                        console.dir(data.review[0].body);
                         const pagination = data['$'];
                         const reviews = data.review.map((r) => r.id);
                         const urls = data.review.map((r) => r.url);
-                        const books = data.review.map((r) => {
-                            console.log(r.date_added);
-                            return {
-                                title: r.book[0].title[0],
-                                author: r.book[0].authors[0].author[0].name[0],
-                                reviewId: r.id[0],
-                                reviewUrl: r.url[0],
-                                date: r.date_added[0]
-                            }
+                        const books = data.review.map((r) => ({
+                            title: r.book[0].title[0],
+                            author: r.book[0].authors[0].author[0].name[0],
+                            reviewId: r.id[0],
+                            reviewUrl: r.url[0],
+                            date: r.date_added[0]
                         });
-                        // console.log({ reviews, urls });
-                        // console.log(books);
-                        console.log({ pagination })
-                        // console.log(data)
-                        // bookIds = books.map((book) => book.reviewId);
+
                         allBooks = allBooks.concat(books);
-                        // return bookIds
-                        console.log({ allBooks });
 
                         if (pagination.end === pagination.total) {
-                        // if (i === 1) {
-                            console.log("BOOKS:", books);
                             scrapeReviews(req, res, allBooks);
                         } else {
                             i += 1;
+
+                            // adhere to rate limiting on goodreads api
                             setTimeout(() => {
                                 go(i);
                             }, 1100)
@@ -121,7 +114,7 @@ app.get('/review', (req, res) => {
                         res.send(JSON.stringify(r.GoodreadsResponse.review[0].body));
                     });
 
-                    // this will be the raw XML
+                    // this will be the raw XML; enable for debug
                     // console.log(rawData);
                 } catch (e) {
                     console.error('e:', e.message);
@@ -136,7 +129,6 @@ app.get('/review', (req, res) => {
 
 function scrapeReviews(req, res, books) {
     const reviews = books.map((book) => book.reviewId);
-    console.log("REVIEWS:", reviews);
     let reviewsData = [];
 
     // todo: this should just be books to scrape all
@@ -156,70 +148,58 @@ function scrapeReviews(req, res, books) {
                 }
             }).then(({ data, response }) => {
                 if (!data || !data.review || !data.review.length) {
+                    // do not continue if we do not have good data to parse
                     resolve();
                 }
 
                 // data.review.replace("'", "&apos;");
-                // data.review.replace('"', '&quot;');
-                data.review = encode(data.review);
+                data.review.replace('"', "''");
+                data.review = data.review;
                 data.review += '<br /><br />See this review and more at <a href="' + getUrl(slug) + '">Goodreads</a>.';
                 data.url = getUrl(slug);
-                data.title = encode(data.title);
+                data.title = data.title;
                 reviewsData.push(data);
                 data.date = d;
 
-                if (i === reviews.length - 1) {
-                    // console.log("next scrape is", i + 1);
-                    resolve();
-                } else {
-                    console.log("finished scraping");
-                    resolve();
-                }
+                resolve();
             });
 
             scrape(i);
         });
     });
 
-    const encode = (rawStr) => rawStr.replace(/[\u00A0-\u9999<>\&]/gim, function(i) {
-        return '&#'+i.charCodeAt(0)+';';
-    });
-
     // scrape each review
-    Promise.all(promises).then((r) => {
-        console.log("REVIEWSDATA:", reviewsData);
-        let formattedData = "INSERT INTO `yee_posts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`) VALUES ";
+    Promise.all(promises)
+        .then((r) => {
+            let formattedData = "INSERT INTO `yee_posts` (`ID`, `post_author`, `post_date`, `post_date_gmt`, `post_content`, `post_title`, `post_excerpt`, `post_status`, `comment_status`, `ping_status`, `post_password`, `post_name`, `to_ping`, `pinged`, `post_modified`, `post_modified_gmt`, `post_content_filtered`, `post_parent`, `guid`, `menu_order`, `post_type`, `post_mime_type`, `comment_count`) VALUES ";
 
-        reviewsData.forEach((data, i) => {
-            let { review, title, author, url, date } = data;
-            title = encode(title);
+            reviewsData.forEach((data, i) => {
+                let { review, title, author, url, date } = data;
+                let fixedTitle = removeSymbols(title);
 
-            const mmt = moment(date);
-            const formatted = mmt.format("YYYY-MM-DD HH:MM:SS");
-            var month = mmt.month() + 1;
-            var day = mmt.date();
-            var year  = mmt.year();
+                const mmt = moment(date);
+                const formatted = mmt.format("YYYY-MM-DD HH:MM:SS");
+                var month = mmt.month() + 1;
+                var day = mmt.date();
+                var year  = mmt.year();
 
-            // latest wordpress post id
-            const currentPostNumber = 1949;
+                // latest wordpress post id
+                const currentPostNumber = 1949;
 
-            // dates to go through
-            const earliest = "10/18/2017 00:00:00";
-            const latest = "05/24/2019 00:00:00"
+                // dates to go through
+                const earliest = "10/18/2017 00:00:00";
+                const latest = "05/24/2019 00:00:00"
 
-            if (mmt.isBetween(earliest, latest)) {
-                formattedData += (
-                    `(${ currentPostNumber + i },2,'${ formatted }','${ formatted }','${ review }','${ title }','','publish','open','open','','${ title.split(" ").join("-").replace("'", "") }','','','${ formatted }','${ formatted }','',0,'https://www.sweetheartseer.com/?p=${ currentPostNumber + i }',0,'post','',0),`
-                );
-            }
-        });
+                if (mmt.isBetween(earliest, latest)) {
+                    formattedData += (
+                        `(${ currentPostNumber + i },2,'${ formatted }','${ formatted }','${ review }',"${ title }",'','publish','open','open','','${ fixedTitle.split(" ").join("-").replace("'", "") }','','','${ formatted }','${ formatted }','',0,'https://www.sweetheartseer.com/?p=${ currentPostNumber + i }',0,'post','',0)${ i !== reviewsData.length - 1 ? "," : "" }`
+                    );
+                }
+            });
 
-        console.log("FORMATTED DATA:", formattedData);
-
-        // date format col 256 is http://www.sweetheartseerbooks.com/2019/06/01/becoming-by-michelle-obama/
-        res.setHeader('content-type', 'text/plain');
-        res.send(formattedData);
-    }, (e) => console.log({ e }))
+            res.setHeader('content-type', 'text/plain');
+            res.send(formattedData);
+        }, (e) => console.log({ e }))
         .catch((error) => console.log({ error }));
 }
 
